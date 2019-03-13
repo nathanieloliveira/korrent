2e3ee4475c629e44f7857a8faf2a2f90be4ac095
Nathaniel Salvador de Oliveira <nathanieloliveira@yahoo.com.br>
2019-03-13 00:32:02 -0300

    added license to source files

--- src/commonMain/kotlin/com/korrent/Korrent.kt
+++ src/commonMain/kotlin/com/korrent/Korrent.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 Nathaniel Salvador de Oliveira
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.korrent
 
 import kotlinx.coroutines.*
@@ -5,8 +22,6 @@ import kotlinx.coroutines.channels.SendChannel
 import kotlinx.io.core.ByteReadPacket
 import kotlinx.io.core.IoBuffer
 import kotlinx.io.core.readLongLittleEndian
-import kotlinx.io.core.use
-import kotlinx.io.pool.useBorrowed
 import kotlinx.io.pool.useInstance
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
--- src/commonMain/kotlin/com/korrent/bencode/BenReader.kt
+++ src/commonMain/kotlin/com/korrent/bencode/BenReader.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 Nathaniel Salvador de Oliveira
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.korrent.bencode
 
 import kotlinx.serialization.SharedImmutable
@@ -56,7 +73,7 @@ private fun ByteArray.initC2TC(c: Char, cl: Byte) {
 }
 
 private val Char.isNumeral: Boolean
-        get() = this in '0'..'9'
+    get() = this in '0'..'9'
 
 internal fun charToTokenClass(c: Char) = if (c.toInt() < CTC_MAX) C2TC[c.toInt()] else TC_OTHER
 
